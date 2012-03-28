84f8062fd05e161522886cc4140216896f963f7e
Nathan Wells <nwwells@gmail.com>
2012-03-28 16:45:09 -0600

    Added submit_job function in jobs.php

--- jobs.php
+++ jobs.php
@@ -8,11 +8,13 @@ class MwsAuthenticationException extends Exception {};
 class MwsAuthorizationException extends Exception {};
 # for unauthorized users
 class UserAuthorizationException extends Exception {};
+# general MWS Error code exception
+class MwsErrorCodeException extends Exception {};
 
 # Define configuration constants
-if (!defined('MWS_SCHEME')) define('MWS_SCHEME', 'https');
+if (!defined('MWS_SCHEME')) define('MWS_SCHEME', 'http');
 if (!defined('MWS_HOST')) define('MWS_HOST', 'localhost');
-if (!defined('MWS_PORT')) define('MWS_PORT', 8443);
+if (!defined('MWS_PORT')) define('MWS_PORT', 8080);
 if (!defined('MWS_BASE')) define('MWS_BASE', '/mws/rest/');
 if (!defined('MWS_USER')) define('MWS_USER', 'admin');
 if (!defined('MWS_PASS')) define('MWS_PASS', 'adminpw');
@@ -31,13 +33,15 @@ function run_curl($resource, $config_closure=null, $info_store=null) {
   curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
   curl_setopt($ch, CURLOPT_USERPWD, $userpwd);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+
   # TODO - this is improper from a security perspective, since
   # we essentially trust any certificate and, therefore, identity
   # of the MWS server is not guaranteed
   #
   # However, since we can rely on DNS being configured properly
   # at this point, it's not 100% necessary to fix right now.
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  #curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
   # These are stubs for the correct (secure) settings. Thanks to 
   # http://unitstep.net/blog/2009/05/05/using-curl-in-php-to-access-https-ssltls-protected-sites/
@@ -51,8 +55,12 @@ function run_curl($resource, $config_closure=null, $info_store=null) {
 
   if($info_store['http_code'] == 401) {
     throw new MwsAuthenticationException('MWS has refused access to the configured username/password');
+  } else if ($info_store['http_code'] >= 400) {
+    error_log('error when calling MWS!');
+    error_log('response: '.$resp);
+    error_log('info: '.json_encode($info_store));
+    throw new MwsErrorCodeException('Call to MWS failed with error '.$info_store['http_code']);
   }
-
   return json_decode($resp);
 }
 
@@ -83,6 +91,16 @@ function get_jobs($username) {
   $jobs_resp->totalCount = $jobs_resp->resultCount;
 
   return $jobs_resp;
+}
+
+function submit_job($job) {
+  if (!is_string($job))
+    $job = json_encode($job);
+
+  return run_curl("jobs", function($ch) use ($job) {
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $job);
+  });
 }
 
 function check_user($username, $job, $silent=false) {
